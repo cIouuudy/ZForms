bf133fa8bec514f4290050a00effb0c3faaa3d4c
cIouuudy <zenanauigliana@gmail.com>
2025-04-05 18:24:00 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAvRGbFNtrFB-2Os8ntTHYYEsRiTldne5w",
+  authDomain: "zforms-a1865.firebaseapp.com",
+  databaseURL: "https://zforms-a1865-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "zforms-a1865",
+  storageBucket: "zforms-a1865.firebasestorage.app",
+  messagingSenderId: "337252242236",
+  appId: "1:337252242236:web:e3589caaeff0255219c439"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ myscript.js
@@ -0,0 +1,88 @@
+var rollV, nameV, genderV, addressV, courseV, emailV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  courseV = document.getElementById("course").value;
+  emailV = document.getElementById("email").value;
+  console.log(rollV, nameV, addressV, genderV, courseV, emailV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      course: courseV,
+      email: emailV,
+    });
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("course").value = "";
+  document.getElementById("email").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+      document.getElementById("course").value = snap.val().course;
+      document.getElementById("email").value = snap.val().email;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      course: courseV,
+      email: emailV,
+    });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("course").value = "";
+  document.getElementById("email").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("course").value = "";
+  document.getElementById("email").value = "";
+};
